71f114276995cb36864def2b5700087d6dedc6e6
nuochka <140435213+nuochka@users.noreply.github.com>
2024-02-25 18:01:08 +0100

    Spring Boot initialization was added

---
+++ src/main/java/com/PetHealthHub/PetHealthHubApplication.java
@@ -0,0 +1,13 @@
+package com.PetHealthHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetHealthHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetHealthHubApplication.class, args);
+	}
+
+}
