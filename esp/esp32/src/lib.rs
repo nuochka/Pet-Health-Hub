da5322c36d80dee2e1b676402045e2d84c50e8b5
Notforest <notforest69@gmail.com>
2024-03-08 00:13:53 +0100

    Added a library space for esp32 target.

---
+++ esp/esp32/src/lib.rs
@@ -0,0 +1,9 @@
+#![feature(custom_test_frameworks, used_with_arg)]
+#![allow(non_snake_case)]
+#![no_std]
+
+//! Main library entry for PHH firmware for esp32 target.
+//!
+//! This library provides all functionality related to PHH service. All sensors which are
+//! supported by the target. This library do not guarantee the functionality on all espressif
+//! devices and so must be compiled and written to the board by an installer.
